6acec4916b9c3953ec6ca6e7121d79bd5bc9a186
Felipe Belloni <141536766+yBelloni@users.noreply.github.com>
2024-10-23 19:43:19 +0000

    Atualização 23/10 16:43

--- assets/darkmode.js
+++ assets/darkmode.js
@@ -2,10 +2,23 @@ const check = document.getElementById('chk');
 check.addEventListener('click', () => {
   document.body.classList.toggle('dark');
   
-  if (document.body.classList.contains('dark')) {
+if (document.body.classList.contains('dark')) {
     document.querySelector("#imagem").src = "assets/imgs/logo/1-removebg-preview.png";
-  } else {
+} else {
     document.querySelector("#imagem").src = "assets/imgs/logo/2-removebg-preview.png";
-  }
+}
 });
+
+const text = "Developer & Editor";
+const paragrafo_typing = document.getElementById("paragrafo_typing");
+let index = 0;
+
+function type() {
+    if (index < text.length) {
+        paragrafo_typing.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100);
+      }
+}
+type();
 
